7f3a8748d7c7be5bebf2875a004fba71d2684d87
albertusmarco <39873755+albertusmarco@users.noreply.github.com>
2019-03-29 16:44:57 +0700

    marcoooo

--- GamePractice/MainMenuScene.swift
+++ GamePractice/MainMenuScene.swift
@@ -7,6 +7,9 @@
 //
 
 import SpriteKit
+import SceneKit
+import AVKit
+import ARKit
 
 class MainMenuScene: SKScene {
     
@@ -16,8 +19,31 @@ class MainMenuScene: SKScene {
     private var bestScoreLabel:SKLabelNode!
     private var scoreLabel : SKLabelNode?
     private var bestScore:Int = 0
+    private var background: SKSpriteNode?
+    
+    var mainGuy = SKSpriteNode()
+    var TextureAtlas = SKTextureAtlas()
+    var TextureArray = [SKTexture]()
     
     override func didMove(to view: SKView) {
+        TextureAtlas = SKTextureAtlas(named: "seq")
+        
+        for i in 1...TextureAtlas.textureNames.count {
+            let name = "\(i).png"
+            TextureArray.append(SKTexture(imageNamed: name))
+            print(name)
+        }
+        
+//        mainGuy = SKSpriteNode(imageNamed: TextureAtlas.textureNames[0])
+//
+//        mainGuy.size = CGSize(width: 70, height: 140)
+//        mainGuy.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+//        self.addChild(mainGuy)
+        
+        background = self.childNode(withName: "background") as? SKSpriteNode
+        background = SKSpriteNode(imageNamed: TextureAtlas.textureNames[0])
+        background!.run(SKAction.repeatForever(SKAction.animate(with: TextureArray, timePerFrame: 0.1)))
+        
         tuneYourLabel = self.childNode(withName: "label_tune_your") as? SKLabelNode
         titleLabel = self.childNode(withName: "label_pitch") as? SKLabelNode
         
@@ -40,6 +66,7 @@ class MainMenuScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        background!.run(SKAction.repeatForever(SKAction.animate(with: TextureArray, timePerFrame: 0.1)))
         for t in touches {
             self.touchDown(atPoint: t.location(in: self))
         }
